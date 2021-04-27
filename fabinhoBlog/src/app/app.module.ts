2ec3e967617fb1f3fdd6c65c634daf66a0eea461
unknown <fabiolimadiogenes@gmail.com>
2021-04-27 13:11:20 -0300

    Implementando rodapé e menu do blog pessoal

--- fabinhoBlog/src/app/app.module.ts
+++ fabinhoBlog/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    RodapeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
